0383e2f5faef451a7825a36471047432062b0760
Varun <varun04.vangala@gmail.com>
2024-07-26 22:32:19 +0530

    Minor change

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 //middleware
 
@@ -17,10 +17,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts',postRoutes);  // every route inside of postRoutes , will start with /post (basically adding a prefix )
 
-const CONNECTION_URL = 'mongodb+srv://MYSTE123:B6AkRm%25VEKHtRD7@cluster0.ktkgyxr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true,useUnifiedTopology:true} )
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
